a422f9935d47d5d3988a4136b0f15a67d6a57fd4
汤启顺 <447847739@qq.com>
2019-05-12 21:51:18 +0800

    在test checkProjectsRange.java中针对单元测试用例增加了几条

--- src/test/java/com/softtest/softtest/ControlUtilTest/checkProjectsRange.java
+++ src/test/java/com/softtest/softtest/ControlUtilTest/checkProjectsRange.java
@@ -33,9 +33,8 @@ public class checkProjectsRange {
         projectName.add("2234");
         map = new HashMap<>();
         noMap = new HashMap<>();
-        map.put("单选题", initList("测试技术"));
-        map.put("情景题", initList("test02"));
-        map.put("图像题", initList("2234"));
+
+
         noMap.put("单选题", initList("22"));
         noMap.put("情景题", initList("33"));
         noMap.put("图像题", initList("223"));
@@ -43,8 +42,48 @@ public class checkProjectsRange {
     }
 
     @Test
-    public void testExist() {
+    public void testExist1() {
         try {
+            map.put("单选题", initList("测试技术"));
+            controlUtil.checkProjectsRange(map, projectName);
+        } catch (BeyondProjectsException e) {
+            fail();
+        }
+    }
+    @Test
+    public void testExist2() {
+        try {
+            map.put("情景题", initList("test02"));
+            controlUtil.checkProjectsRange(map, projectName);
+        } catch (BeyondProjectsException e) {
+            fail();
+        }
+
+    }
+
+
+    @Test
+    public void testExist3() {
+        try {
+            map.put("图像题", initList("2234"));
+            controlUtil.checkProjectsRange(map, projectName);
+        } catch (BeyondProjectsException e) {
+            fail();
+        }
+    }
+    @Test
+    public void testExist4() {
+        try {
+            map.put("图像题", initList("test1"));
+            controlUtil.checkProjectsRange(map, projectName);
+        } catch (BeyondProjectsException e) {
+            fail();
+        }
+    }
+    @Test
+    public void testExist5() {
+        try {
+            map.put("图像题", initList("test03"));
             controlUtil.checkProjectsRange(map, projectName);
         } catch (BeyondProjectsException e) {
             fail();
@@ -53,9 +92,49 @@ public class checkProjectsRange {
 
 
     @Test
-    public void testNoExist() {
+    public void testNoExist1() {
 
         try {
+            controlUtil.checkProjectsRange(noMap, projectName);
+            fail();
+        } catch (BeyondProjectsException e) {
+            assertEquals("题目在制定项目范围外", e.getDetails());
+        }
+    }    @Test
+    public void testNoExist2() {
+
+        try {
+            noMap.put("图像题", initList("test11"));
+            controlUtil.checkProjectsRange(noMap, projectName);
+            fail();
+        } catch (BeyondProjectsException e) {
+            assertEquals("题目在制定项目范围外", e.getDetails());
+        }
+    }    @Test
+    public void testNoExist3() {
+
+        try {
+            noMap.put("图像题", initList("test112"));
+            controlUtil.checkProjectsRange(noMap, projectName);
+            fail();
+        } catch (BeyondProjectsException e) {
+            assertEquals("题目在制定项目范围外", e.getDetails());
+        }
+    }    @Test
+    public void testNoExist4() {
+
+        try {
+            noMap.put("图像题", initList("test113"));
+            controlUtil.checkProjectsRange(noMap, projectName);
+            fail();
+        } catch (BeyondProjectsException e) {
+            assertEquals("题目在制定项目范围外", e.getDetails());
+        }
+    }    @Test
+    public void testNoExist5() {
+
+        try {
+            noMap.put("图像题", initList("test114"));
             controlUtil.checkProjectsRange(noMap, projectName);
             fail();
         } catch (BeyondProjectsException e) {
